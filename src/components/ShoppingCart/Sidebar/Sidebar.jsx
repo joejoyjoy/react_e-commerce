064f8ff8e022131cf1162e3e04fdc9089aece97d
Joe Alt <joejoyalt@gmail.com>
2023-02-18 15:50:09 +0100

    Add extract structure

--- src/components/ShoppingCart/Sidebar/Sidebar.jsx
+++ src/components/ShoppingCart/Sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = ({ sidebar, closeSidebar }) => {
             <section className="sidebar-container margin-bottom-0">
                 <TfiClose className="icon pointer" size="1.7rem" onClick={closeSidebar} />
                 <h4 className="sidebar-title">My shopping cart</h4>
-                <span className="sidebar-small-text">1 product</span>
+                <span className="sidebar-small-text">2 products</span>
                 <h6 className="sidebar-text">Added to cart</h6>
                 <div className="sidebar-product">
                     <div className="sidebar-product-img">
@@ -16,9 +16,10 @@ const Sidebar = ({ sidebar, closeSidebar }) => {
                     </div>
                     <div className="sidebar-product-details">
                         <h5 className="sidebar-product-title">Sofa title</h5>
+                        <span className="sidebar-small-text">Quantity: 1</span>
                         <div className="sidebar-product-price">
-                            <span className="sidebar-small-text">Quantity: 1</span>
-                            <span className="sidebar-large-text">P970€</span>
+                            <span className="sidebar-large-text">- 1 +</span>
+                            <span className="sidebar-large-text">P320€</span>
                         </div>
                     </div>
                 </div>
@@ -28,21 +29,10 @@ const Sidebar = ({ sidebar, closeSidebar }) => {
                     </div>
                     <div className="sidebar-product-details">
                         <h5 className="sidebar-product-title">Sofa title</h5>
+                        <span className="sidebar-small-text">Quantity: 2</span>
                         <div className="sidebar-product-price">
-                            <span className="sidebar-small-text">Quantity: 1</span>
-                            <span className="sidebar-large-text">P970€</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="sidebar-product">
-                    <div className="sidebar-product-img">
-                        <img src="#" width="100px" height="100px" />
-                    </div>
-                    <div className="sidebar-product-details">
-                        <h5 className="sidebar-product-title">Sofa title</h5>
-                        <div className="sidebar-product-price">
-                            <span className="sidebar-small-text">Quantity: 1</span>
-                            <span className="sidebar-large-text">P970€</span>
+                            <span className="sidebar-large-text">- 2 +</span>
+                            <span className="sidebar-large-text">P167€</span>
                         </div>
                     </div>
                 </div>
@@ -51,9 +41,9 @@ const Sidebar = ({ sidebar, closeSidebar }) => {
                 <div className="sidebar-container-flex">
                     <div className="sidebar-subtotal">
                         <span className="sidebar-large-text">Order Subtotal</span>
-                        <span className="sidebar-large-text">P970€</span>
+                        <span className="sidebar-large-text">P654€</span>
                     </div>
-                    <span className="sidebar-small-text">Delivery time 1-4 working days.</span>
+                    <span className="sidebar-small-text">Delivery time 10-14 working days.</span>
                     <button className="sidebar-btn pointer" onClick={closeSidebar}>CONTINUE SHOPPING</button>
                 </div>
             </div>
